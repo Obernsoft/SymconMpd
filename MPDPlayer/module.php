4989a39ca27e015046212cc1415ba60d81a14b08
Joerg Oberndorf <joerg.oberndorf@obernsoft.de>
2019-09-28 15:29:16 +0200

    add code

--- MPDPlayer/module.php
+++ MPDPlayer/module.php
@@ -1,26 +1,42 @@
 <?
-	class MpdPlayer extends IPSModule
+	class MPDPlayer extends IPSModule
 	{
+		public create {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+
+			$this->RegisterPropertyString("IPAddress", "");
+			$this->RegisterPropertyString("Host", "");
+			$this->RegisterPropertyString("Password", "");
+
+			$this->RegisterTimer("KeepAliveTimer", 30000, 'MPDPlayer_KeepAlive()');
+		}
 
 		public function ApplyChanges()
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
 
+			$this->RegisterVariableBoolean("Power", "Power", "~Switch",1);
+			$this->EnableAction("Power");
+
+			$this->RegisterVariableString("Titel", "Titel","",2);
+
+
+			$this->RegisterProfileIntegerEx("Mpd.Status", "Information", "", "", Array( Array(0, " << ", "", -1),
+																					Array(1, " Stop ",   "", -1),
+																					Array(2, " Pause ",  "", -1),
+																					Array(3, " Play ",   "", -1),
+																					Array(4, " >> ",    "", -1) ));
+
+			$this->RegisterVariableInteger("Status","Status","Mpd.Status",3);
+			SetValue($this->GetIDForIdent("Status"), 1);
+			$this->EnableAction("Status");
+
 			//Connect to available splitter or create a new one
-			$this->ConnectParent("{0749F7C9-2EE0-444F-91D5-6450D494208F}");
-
-			$this->RegisterTimer("KeepAliveTimer", 60000, 'MpdPlayer_KeepAlive()');
-
+			$this->ForceParent("{0749F7C9-2EE0-444F-91D5-6450D494208F}");
 		}
 
-		/**
-		* This function will be available automatically after the module is imported with the module control.
-		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
-		*
-		* IOT_Send($id, $text);
-		*
-		*/
 		public function Send($Text)
 		{
 			$this->SendDataToParent(json_encode(Array("DataID" => "{B87AC955-F258-468B-92FE-F4E0866A9E18}", "Buffer" => $Text)));
@@ -29,7 +45,7 @@
 		public function ReceiveData($JSONString)
 		{
 			$data = json_decode($JSONString);
-			IPS_LogMessage("IOTest", utf8_decode($data->Buffer));
+			IPS_LogMessage("MPDPlayer", utf8_decode($data->Buffer));
 			//Parse and write values to our variables
 			echo $data;
 		}
